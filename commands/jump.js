876852b047fcb0595e1441b98c5bff8b37ea8ce2
Shkiper_UASH <vovamyrosh2003@gmail.com>
2022-08-23 20:28:12 +0300

    add jump-music command

--- commands/jump.js
+++ commands/jump.js
@@ -1,5 +1,7 @@
 const Command = require('../classes/Command.js');
 const log = require('../classes/Logger.js');
+const { checkAndConvertOfType } = require('../utils/stringAndNumsFormat.js');
+const ErrorAlarm = require('../classes/ErrorAlarm.js');
 
 const jump = new Command(client, {
     name: 'jump',
@@ -30,7 +32,22 @@ const jump = new Command(client, {
             return;
         } else {
             const queue = client.player.getQueue(client.guild);
-            console.log(queue.tracks);
+
+            const num = checkAndConvertOfType(args[0], 'int');
+            if(num) {
+                try {
+                    queue.just(num)
+                } catch (error) {
+                    log(`Не вдалось перестрибнути на іншу позицію трека музичної черги\nПомилка: ${error}`, 'error');
+                }
+            } else {
+                new ErrorAlarm({
+                    description: 'Ви повинні ввести ціле число',
+                    timeout: 10,
+                    hexColor: '#ffff00',
+                    channel: message.channel
+                })
+            } 
         }
     }
 })
