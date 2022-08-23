54b41c5b146ea7ed3642246a7ca425a7b3916025
Shkiper_UASH <vovamyrosh2003@gmail.com>
2022-08-23 23:49:04 +0300

    add jump-music command

--- commands/div.js
+++ commands/div.js
@@ -7,7 +7,6 @@ const div = new Command(client, {
     ownerOnly: false,
     adminOnly: false
 }, async (client, message, args) => {
-    client.connection.query('ALTER TABLE members ADD last_divination_date VARCHAR(19) NULL')
     client.connection.query(`SELECT * FROM members WHERE id = ${message.author.id}`, (error, rows) => {
         const current_date = new Date().toLocaleDateString('uk-UA', { timeZone: 'Europe/Kiev' });
         log(current_date);
@@ -17,7 +16,6 @@ const div = new Command(client, {
                 description: 'Ну тобі буде нормально'
             }]})
         }
-        
     })
 })
 
