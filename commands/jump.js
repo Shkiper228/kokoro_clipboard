42eb7a7f28b5ade3fbaa89523eded9d811dbd861
Shkiper_UASH <vovamyrosh2003@gmail.com>
2022-08-23 20:17:16 +0300

    add jump-music command

---
+++ commands/jump.js
@@ -0,0 +1,38 @@
+const Command = require('../classes/Command.js');
+const log = require('../classes/Logger.js');
+
+const jump = new Command(client, {
+    name: 'jump',
+    description: 'Команда що перестрибувати на певну позицію в музичній черзі',
+    syntax: `${client.config.prefix}jump <позиція трека>`,
+    ownerOnly: false,
+    adminOnly: false
+}, async (client, message, args) => {
+    const member = await client.guild.members.fetch(message.author.id);
+    const memberBot = await client.guild.members.fetch(client.user.id);
+
+    if(member.voice.channelId != memberBot.voice.channelId){
+        new ErrorAlarm({
+            description: 'Ви повинні бути в одному голосовому каналі з ботом, аби давати йому команди',
+            timeout: 10,
+            hexColor: '#ffff00',
+            channel: message.channel
+        })
+        return;
+    } else {
+        if(!client.player.getQueue(client.guild)){ //нема черги
+            new ErrorAlarm({
+                description: 'Спочатку створіть музичну чергу',
+                timeout: 10,
+                hexColor: '#ffff00',
+                channel: message.channel
+            })
+            return;
+        } else {
+            const queue = client.player.getQueue(client.guild);
+            console.log(queue.tracks);
+        }
+    }
+})
+
+module.exports = jump;
